5d2f08042f981f3084e0065c9a9d05c4ff427119
Kent Hervey <41525831+kent-hervey@users.noreply.github.com>
2021-02-28 15:05:29 -0600

    Implemented inner loop count on Insertion Sort

--- src/InsertionSorting.java
+++ src/InsertionSorting.java
@@ -12,14 +12,15 @@ public class InsertionSorting {
 			System.out.println("Still at top, we have selected this element to place: " + elements[j] + "; with loop index j of:  " + j);
 			System.out.println("sorted portion of array is:  " + printSubArray(elements, j));
 			int temp = elements[j];
-			int possibleIndex = j; // count these in question 16
-			possibleIndexCounter++;
+			int possibleIndex = j; 
+
 			while (possibleIndex > 0 && temp < elements[possibleIndex - 1]) { //j is greater than 0 and 
 					//the element we are holding is less than the spot checking in sorted array
 				elements[possibleIndex] = elements[possibleIndex - 1]; 
 					//we are starting at the right of the sorted array and looking leftward until we find the spot
 					//when spot is found, while loop breaks and that becomes the index to stick it in
-				possibleIndex--;
+				possibleIndex--; // count these in question 16
+				possibleIndexCounter++;
 			}
 			elements[possibleIndex] = temp; // count these in question 17
 			tempAssignedCounter++;  //to do the count of what was requested in question 17  
